564018ed3f3d01f50a372ade43f0f8f864b98ba6
rafitalavi <rafitalavi12345@gmail.com>
2025-08-26 23:51:44 +0600

    first commit

---
+++ users/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.deconstruct import deconstructible
+
+
+import os
+# Create your models here.
+@deconstructible
+class UploadToPathAndRename:
+    def __init__(self, path):
+        pass
+
+    def __call__(self, instance, filename):
+        ext = filename.split('.')[-1]
+        path = f'media/accounts/{instance.user.id}/images/'
+        filename = f'profile_image.{ext}'
+        return os.path.join(path, filename)
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to=UploadToPathAndRename('media/accounts/'), blank=True, null=True)
+   
+   
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
